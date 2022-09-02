fb66e27a15b8d5226992097359638c35b382cf4f
Manoj <manojpatil1033@gmail.com>
2022-09-02 20:41:30 +0530

    Testcase Data Added

--- src/test/java/Facebook/ValidateFacebookPage.java
+++ src/test/java/Facebook/ValidateFacebookPage.java
@@ -1,3 +1,5 @@
+//Smoke Testcases
+
 package Facebook;
 
 import java.io.IOException;
--- src/test/java/Facebook/ValidateUserNameAndPassword.java
+++ src/test/java/Facebook/ValidateUserNameAndPassword.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+//Smoke Testcases
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
